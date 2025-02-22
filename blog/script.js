332295f27c07ce90a1e950f9331fed60bb4eec04
idk-someone <teodoradelina20@gmail.com>
2025-02-22 02:04:38 +0200

    Add files via upload

---
+++ blog/script.js
@@ -0,0 +1,24 @@
+
+function OpenArtciles()
+{
+
+    window.location.href = "index.html";
+}
+
+function OpenContact()
+{
+
+    window.location.href = "contact.html";
+}
+
+
+function OpenReviews()
+{
+
+    window.location.href = "reviews.html";
+}
+
+
+
+
+
